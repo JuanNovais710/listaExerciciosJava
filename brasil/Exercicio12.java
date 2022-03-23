b13fe599d9c16403722b11f1caff3e33c2c73bec
JuanNovais710 <juannovais63@gmail.com>
2022-03-23 03:50:48 -0300

    Novos

---
+++ brasil/Exercicio12.java
@@ -0,0 +1,14 @@
+package exercicios.python.brasil;
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite sua altura: ");
+            double alturaPessoa = scanner.nextDouble();
+            double pesoIdeal = 72.7 * alturaPessoa - 58.;
+            System.out.println("Seu peso ideal seria: " + pesoIdeal);
+        }
+}
